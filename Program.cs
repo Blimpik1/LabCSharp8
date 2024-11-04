affd22a056ad591735b0dc6146c010fce393b7ea
Владислав Микитчук <Mykytchuk_Vl@ifnmu.edu.ua>
2024-11-04 14:54:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    
+    static async Task<string> GetDataFromDatabaseAsync()
+    {
+        Console.WriteLine("Підключення до бази даних...");
+
+        
+        await Task.Delay(2000); 
+
+        Console.WriteLine("Отримання даних з бази...");
+        return "Результат запиту до бази даних";
+    }
+
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Запуск запиту до бази даних...");
+
+       
+        string result = await GetDataFromDatabaseAsync();
+
+        
+        Console.WriteLine($"Результат: {result}");
+    }
+}
